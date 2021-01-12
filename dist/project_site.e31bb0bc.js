3f9892c55050601fec578852372b1164e41b87bb
catherine turra <cathturra@hotmail.com>
2021-01-12 23:02:12 +1100

    newcommit

--- dist/project_site.e31bb0bc.js
+++ dist/project_site.e31bb0bc.js
@@ -2743,7 +2743,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51580" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55041" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
